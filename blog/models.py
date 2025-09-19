12bb3c8318c3136c202658a8e4d5745acebab8ec
curtisnlogan <curtisnlogan@gmail.com>
2025-09-19 19:34:58 +0100

    fix: correct capitalization of 'approved' field in Comment model

--- blog/models.py
+++ blog/models.py
@@ -26,7 +26,7 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commentor")
     body = models.TextField()
-    Approved = models.BooleanField(default=False)
+    approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
