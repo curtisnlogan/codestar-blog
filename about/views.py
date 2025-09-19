e2d2b70285215d06ddcf89cff51a6dd4d57974be
curtisnlogan <curtisnlogan@gmail.com>
2025-09-19 17:47:39 +0100

    feat: update about view to handle collaboration requests and improve documentation

--- about/views.py
+++ about/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from .models import About
 from .forms import CollaborateForm
+from django.contrib import messages
 
 # Create your views here.
 
 
 def about(request):
     """
-    Display the About page content.
+    Display About page content and handle collaboration requests.
 
-    Retrieves the first About instance from the database and renders
-    it on the about page.
+    Retrieves the About instance from the database and renders
+    it on the about page along with the collaboration form.
+    Handles POST requests for collaboration form submission.
 
     **Context**
 
     ``about``
         An instance of :model:`about.About`.
+    ``collaborate_form``
+        An instance of :model:`about.CollaborateRequest`
 
     **Template:**
 
@@ -23,10 +27,23 @@ def about(request):
     """
 
     about = get_object_or_404(About)
-    collaborate = CollaborateForm()
+
+    if request.method == "POST":
+        print("Received a POST request")
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                "Collaboration request received! I endeavour to respond within 2 working days.",
+            )
+    collaborate_form = CollaborateForm()
+
+    print("About to render template")
 
     return render(
         request,
         "about/about.html",
-        {"about": about, "collaborate_form": collaborate},
+        {"about": about, "collaborate_form": collaborate_form},
     )
